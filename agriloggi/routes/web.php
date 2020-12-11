58fe9eaf26bf2665b13e4898fb411f870f481c92
Vinicius-cocorullo <75094515+Vinicius-cocorullo@users.noreply.github.com>
2020-12-11 20:59:10 -0300

    Update web.php

--- agriloggi/routes/web.php
+++ agriloggi/routes/web.php
@@ -14,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'App\Http\Controllers\PhotoController@index');
+Route::get('cadastro', 'App\Http\Controllers\PhotoController@photoAddUser') ->name('users.photoAddUser');
